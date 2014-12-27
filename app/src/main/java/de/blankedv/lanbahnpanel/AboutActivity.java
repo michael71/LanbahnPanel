89f5b069505c42f89065884b6db45b2cf2b5c228
Michael Blank <mblank@bawue.de>
2014-12-27 13:13:24 +0100

    added gradle files.

--- app/src/main/java/de/blankedv/lanbahnpanel/AboutActivity.java
+++ app/src/main/java/de/blankedv/lanbahnpanel/AboutActivity.java
@@ -2,14 +2,14 @@ package de.blankedv.lanbahnpanel;
 
 /*  (c) 2011, Michael Blank
  * 
- *  This file is part of SRCP Client.
+ *  This file is part of LANBAHN Panel Client.
 
-    SRCP Client is free software: you can redistribute it and/or modify
+    LANBAHN Panel is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    SRCP Client is distributed in the hope that it will be useful,
+    LANBAHN Panel distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
